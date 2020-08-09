ea103c46004612c2b36a0a512c41dd2bb8d4d8fd
Tosin <famurewatosin@gmail.com>
2020-08-09 22:20:20 +0100

    refactor css changes

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -1,8 +1,12 @@
 <x-app>
     <header class="mb-6 relative">
-        <img src="/images/default-profile-banner.jpg" alt="" class="mb-2">
+        <div class="relative">
+            <img src="/images/default-profile-banner.jpg" alt="" class="mb-2">
+            <img src="{{$user->avatar}}" alt="" class="rounded-full mr-2 absolute bottom-0 transform -translate-x-1/2 translate-y-1/2" width="150" style="left: 50%">
+        </div>
 
-    <div class="flex justify-between items-center mb-4">
+
+        <div class="flex justify-between items-center mb-4">
         <div>
             <h2 class="font-bold text-2xl mb-2">{{$user->name}}</h2>
             <p>Joined {{$user->created_at->diffForHumans()}}</p>
@@ -14,7 +18,6 @@
     </div>
         <p class="text-sm">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto aspernatur at quo. Distinctio, rem, unde. A deleniti, eum explicabo facilis incidunt iure perspiciatis placeat porro, praesentium quae quo sint tempora?</p>
 
-        <img src="{{$user->avatar}}" alt="" class="rounded-full mr-2 absolute" style="width: 150px; left: calc(50% - 75px); top: 47%">
     </header>
     <hr>
     @include('_timeline',['tweets' => $user->tweets])
